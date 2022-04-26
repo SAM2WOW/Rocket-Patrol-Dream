e1988a07908612c8140ffc1f9dc68424e75621ad
Jose Salas <jsalvadorsalas000@gmail.com>
2022-04-26 09:18:52 -0700

    Made the font larger to improve visibility on smaller screens

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -22,14 +22,14 @@ class Menu extends Phaser.Scene {
         this.title.setFontFamily('PixelFont');
         this.title.setFontSize(150);
         this.title.setColor('#ffffff');
-        this.title.setStroke('#000000', 5);
+        this.title.setStroke('#000000', 10);
         this.title.setShadow(2, 2, "#333333", 2, false, true);
         this.title.setOrigin(0.5, 0.5);
         this.title.setDepth(1);
 
         // add tutorials
-        this.add.text(game.config.width / 2, game.config.height / 2 - 150, "hope you have a ...", {fontFamily: 'PixelFont', fontSize: 50}).setOrigin(0.5, 0.5).setStroke('#000000', 5).setShadow(2, 2, "#333333", 2, false, true);;
-        this.add.text(game.config.width / 2, game.config.height / 2 + 300, "[←] [→] Move | [F] Fire | [F11] Toggle Fullscreen", {fontFamily: 'PixelFont', fontSize: 30}).setOrigin(0.5, 0.5).setStroke('#000000', 5).setShadow(2, 2, "#333333", 2, false, true);;
+        this.add.text(game.config.width / 2, game.config.height / 2 - 150, "hope you have a ...", {fontFamily: 'PixelFont', fontSize: 75}).setOrigin(0.5, 0.5).setStroke('#000000', 7).setShadow(2, 2, "#333333", 2, false, true);;
+        this.add.text(game.config.width / 2, game.config.height / 2 + 300, "[←] [→] Move | [F] Fire | [F11] Toggle Fullscreen", {fontFamily: 'PixelFont', fontSize: 40}).setOrigin(0.5, 0.5).setStroke('#000000', 5).setShadow(2, 2, "#333333", 2, false, true);;
         this.add.text(game.config.width / 2, game.config.height / 2 + 350, "[←] for Novice | [→] for Expert", {fontFamily: 'PixelFont', fontSize: 50}).setOrigin(0.5, 0.5).setStroke('#000000', 5).setShadow(2, 2, "#333333", 2, false, true);;
         
         // define keys
